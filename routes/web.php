b833e18df2d620c9d09ac71ba33b456c8453eda7
Alizon27 <alizoncris27@gmail.com>
2025-03-06 19:03:01 -0600

    Añadida ruta resource para GenderController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GenderController; // Importa el controlador
 
+// Ruta principal
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para GenderController
+Route::resource('genders', GenderController::class);
